1b139f3e8683b289fb940bb2e0e7b07f51921fbd
Bennett G Sewell <bennettgsewell@gmail.com>
2020-12-15 17:06:59 -0500

    Create list of all images in DCIM folders on all connected drives.

---
+++ PD Camera Importer/WindowsFormsApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// This list will contain all the absolute paths to the image files.
+        /// </summary>
+        static List<string> s_allPictureFiles = new List<string>(1024);
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Find the drives that are cameras.
+            //It should be the drive with the DCIM folder at the root of it.
+            string[] drives = Environment.GetLogicalDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                string pathToDCIM = Path.Combine(drives[i], "DCIM");
+                if(Directory.Exists(pathToDCIM))
+                {
+                    LoadAllImages(pathToDCIM);
+                }
+            }
+
+            Application.Run(new ListPicturesForm());
+        }
+
+        static void LoadAllImages(string path)
+        {
+            foreach (string file in Directory.EnumerateFiles(path))
+                s_allPictureFiles.Add(file);
+            foreach (string folder in Directory.EnumerateDirectories(path))
+                LoadAllImages(folder);
+        }
+    }
+}
